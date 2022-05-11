0c003de3ce8f285f79c34bee4358610dd889e853
Winter_Hana <haha920@naver.com>
2022-05-11 18:55:13 +0900

    chapter2

---
+++ 220511Chapter_2/mouse1.py
@@ -0,0 +1,22 @@
+# 마우스로 조종하기
+import pygame
+
+pygame.init()
+
+background = pygame.display.set_mode((480, 360)) 
+pygame.display.set_caption("SONOL") 
+
+
+play = True
+while play:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            play = False
+        if event.type == pygame.MOUSEMOTION:
+            print("MOUSEMOTION")
+        if event.type == pygame.MOUSEBUTTONUP:
+            print("MOUSEBUTTONUP")
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("MOUSEBUTTONDOWN")
+
+pygame.quit()
